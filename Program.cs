bbe8242e73d069c85cbc64e2d8cbf4bf83452f38
Stepuro Mikhail <s1t2e3p4@mail.ru>
2022-11-29 23:08:58 +0300

    Записал методы пребора и вывода в консоль.

--- Program.cs
+++ Program.cs
@@ -3,4 +3,35 @@
 // Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
 // При решение не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
 
+void Main()
+{   
+    Console.Clear();
+    string[] PrimaryArray = new string[4] {"hello", "5", "world", ":-)"};
+    string[] FinalArray = new string[PrimaryArray.Length];
+    CreateResultArrayWithIf(PrimaryArray, FinalArray);
+    PrintArray(FinalArray);
+}
 
+void CreateResultArrayWithIf(string[] PrimaryArray, string[] FinalArray)
+{
+    int count = 0;
+    for (int i = 0; i < PrimaryArray.Length; i++)
+    {
+    if(PrimaryArray[i].Length <= 3)
+        {
+        FinalArray[count] = PrimaryArray[i];
+        count++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+Main();
